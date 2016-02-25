4b88b7dd819c5c58126dbe2b1d9c921f433cc443
murali2805 <murali.sampathirao@gmail.com>
2016-02-25 13:33:17 +0530

    Added files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+exports.PORT = 3001;
+exports.DEBUG = true;
+exports.ENVIRONMENT = 'production';
+exports.CALLBACK_URL = 'http://localhost:3001';
+exports.PUSH_TOPIC = 'AllAccounts';
+
+exports.CLIENT_ID = "YOUR-SFDC-CLIENT-ID"; // Consumer Key
+exports.CLIENT_SECRET = "YOUR-SFDC-CLIENT-SECRET"; // Consumer Secret
+exports.USERNAME = "YOUR-SFDC-USERNAME"; // Salesforce Login Username
+exports.PASSWORD = "YOUR-SFDC-PASSWORD-AND-TOKEN" // Salesforce Password + Token (i.e if abcd is the password and xxxxxx is the security token then this parameter would be: abcdxxxxxx)
